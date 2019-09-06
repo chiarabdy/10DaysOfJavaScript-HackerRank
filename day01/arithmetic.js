2d27f325f239b47430ed06aa7628be65d7faa5fc
chiar abdi <chiar.abdi@powercoders.org>
2019-09-06 21:35:48 +0200

    day01-arithmetic.js

---
+++ day01/arithmetic.js
@@ -0,0 +1,28 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    // Write your code here
+    // area = lenth * width =13.5
+    let area = length * width;
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    // Write your code here
+    let perimeter = 2 * (length + width);
+    return perimeter;
+}
