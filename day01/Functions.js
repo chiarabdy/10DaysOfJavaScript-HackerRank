5d71d1c6286bc1a9c8864d2a0adf94276b41dd5a
chiar abdi <chiar.abdi@powercoders.org>
2019-09-09 23:56:32 +0200

    day01 solutions

---
+++ day01/Functions.js
@@ -0,0 +1,12 @@
+/*
+ *Create the function factorial here
+ */
+
+
+function factorial(n) {
+    let result = n;
+    for (var i = 1; i < n; i++) {
+        result = result * (n - i);
+    }
+    return result;
+}
